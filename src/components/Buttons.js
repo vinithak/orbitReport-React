9333d631998516a48beb36df8a8039e02eb957c1
VInitha <vinivinitha@gmail.com>
2023-08-12 02:11:36 -0500

    Completed Assignment6

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,27 @@
-const Buttons = () => {
+import "./styling.css";
+import satData from "./satData";
+
+const Buttons = (props) => {
+
+  let {filterByType, setSat, displaySats} = props;
+
+
+  const handleClick = () => {
+    setSat(satData);
+  };
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className = "flex-container">
+      {
+        displaySats.map((sat, id) => {
+          return(
+          <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>
+          );
+        })
+      }
+
+      <button onClick={handleClick}>All Orbits</button>
+    </div>
   );
 };
 
